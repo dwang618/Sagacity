7e869c61298f751909613c2dbbf65fb36e7c3c38
Davis Wang <bqe6ue@virginia.edu>
2024-05-24 17:16:23 -0400

    app first rev

--- MentalHealthCalc/app.R
+++ MentalHealthCalc/app.R
@@ -6,48 +6,32 @@
 #
 #    https://shiny.posit.co/
 #
-
 library(shiny)
 data <- read.csv("survey.csv")
+
+# Subset the data to include only specific columns
+selected_columns <- data[, c("Age", "Gender", "Country")]
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  
+  # Application title
+  titlePanel("Survey Data"),
+  
+  # Main panel to show the data table
+  mainPanel(
+    tableOutput("dataTable")  # Add table output here
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    
+    output$dataTable <- renderTable({  # Render the data table
+      selected_columns  # Render the subsetted data table  
     })
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
+
